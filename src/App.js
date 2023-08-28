c565341d1151f3511fe7f499f1baa51ac6fc4f26
Prakash-b1 <pinnub23@gmail.com>
2023-08-28 21:35:04 +0530

    buided

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CartDetails from './components/CartDetails';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes,Route} from "react-router-dom"
-import toast, { Toaster } from 'react-hot-toast';
+import  { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
--- src/components/CartDetails.js
+++ src/components/CartDetails.js
@@ -1,68 +1,71 @@
 import React, { useEffect, useState } from 'react'
 import "./cartstyle.css"
 import { useDispatch, useSelector } from 'react-redux';
-import { addToCart,removeToCart ,removeSingleIteams,emptycartIteam} from '../redux/features/cartSlice';
+import { addToCart, removeToCart, removeSingleIteams, emptycartIteam } from '../redux/features/cartSlice';
 import toast from 'react-hot-toast';
 
 
 const CartDetails = () => {
 
-    const {carts} = useSelector((state)=>state.allCart);
-    
-    const [totalprice,setPrice] = useState(0);
-    const [totalquantity,setTotalQuantity] = useState(0);
+    const { carts } = useSelector((state) => state.allCart);
+
+    const [totalprice, setPrice] = useState(0);
+    const [totalquantity, setTotalQuantity] = useState(0);
 
     const dispatch = useDispatch();
 
     // add to cart
-    const handleIncrement = (e)=>{
+    const handleIncrement = (e) => {
         dispatch(addToCart(e))
     }
 
     // remove to cart
-    const handleDecrement = (e)=>{
+    const handleDecrement = (e) => {
         dispatch(removeToCart(e));
         toast.success("Item Remove From Your Cart")
     }
 
     // remove single item 
-    const handleSingleDecrement = (e)=>{
+    const handleSingleDecrement = (e) => {
         dispatch(removeSingleIteams(e))
     }
 
     // empty cart
-    const emptycart = ()=>{
+    const emptycart = () => {
         dispatch(emptycartIteam())
         toast.success("Your Cart is Empty")
 
     }
 
-    // count total price
-    const total = ()=>{
-        let totalprice = 0
-        carts.map((ele,ind)=>{
-            totalprice = ele.price * ele.qnty + totalprice
-        });
-        setPrice(totalprice)
-    }  
 
-    
-    // count total quantity
-    const countquantity = ()=>{
-        let totalquantity = 0
-        carts.map((ele,ind)=>{
-            totalquantity = ele.qnty + totalquantity
-        });
-        setTotalQuantity(totalquantity)
-    }  
-    
-    useEffect(()=>{
-        total()
-    },[total])
 
-    useEffect(()=>{
-        countquantity()
-    },[countquantity])
+
+
+    useEffect(() => {
+        // count total price
+        const total = () => {
+            let totalprice = 0;
+            carts.map((ele, ind) => {
+                totalprice = ele.price * ele.qnty + totalprice;
+                return null; // Add this return statement
+            });
+            setPrice(totalprice);
+        };
+        total();
+    }, [carts]);
+
+    useEffect(() => {
+        // count total quantity
+        const countquantity = () => {
+            let totalquantity = 0;
+            carts.map((ele, ind) => {
+                totalquantity = ele.qnty + totalquantity;
+                return null; // Add this return statement
+            });
+            setTotalQuantity(totalquantity);
+        };
+        countquantity();
+    }, [carts]);
     return (
         <>
             <div className='row justify-content-center m-0'>
@@ -70,10 +73,10 @@ const CartDetails = () => {
                     <div className="card">
                         <div className="card-header bg-dark p-3">
                             <div className='card-header-flex'>
-                                <h5 className='text-white m-0'>Cart Calculation{carts.length >0 ? `(${carts.length})`:""}</h5>
+                                <h5 className='text-white m-0'>Cart Calculation{carts.length > 0 ? `(${carts.length})` : ""}</h5>
                                 {
                                     carts.length > 0 ? <button className='btn btn-danger mt-0 btn-sm'
-                                    onClick={emptycart}
+                                        onClick={emptycart}
                                     ><i className='fa fa-trash-alt mr-2'></i><span>EmptyCart</span></button>
                                         : ""
                                 }
@@ -81,19 +84,19 @@ const CartDetails = () => {
 
                         </div>
                         <div className="card-body p-0">
-                                {
-                                    carts.length === 0 ? <table className='table cart-table mb-0'>
-                                        <tbody>
-                                            <tr>
-                                                <td colSpan={6}>
-                                                    <div className='cart-empty'>
-                                                        <i className='fa fa-shopping-cart'></i>
-                                                        <p>Your Cart Is Empty</p>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </table> :
+                            {
+                                carts.length === 0 ? <table className='table cart-table mb-0'>
+                                    <tbody>
+                                        <tr>
+                                            <td colSpan={6}>
+                                                <div className='cart-empty'>
+                                                    <i className='fa fa-shopping-cart'></i>
+                                                    <p>Your Cart Is Empty</p>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table> :
                                     <table className='table cart-table mb-0 table-responsive-sm'>
                                         <thead>
                                             <tr>
@@ -107,13 +110,13 @@ const CartDetails = () => {
                                         </thead>
                                         <tbody>
                                             {
-                                                carts.map((data,index)=>{
+                                                carts.map((data, index) => {
                                                     return (
                                                         <>
                                                             <tr>
                                                                 <td>
                                                                     <button className='prdct-delete'
-                                                                    onClick={()=>handleDecrement(data.id)}
+                                                                        onClick={() => handleDecrement(data.id)}
                                                                     ><i className='fa fa-trash-alt'></i></button>
                                                                 </td>
                                                                 <td><div className='product-img'><img src={data.imgdata} alt="" /></div></td>
@@ -121,13 +124,13 @@ const CartDetails = () => {
                                                                 <td>{data.price}</td>
                                                                 <td>
                                                                     <div className="prdct-qty-container">
-                                                                        <button className='prdct-qty-btn' type='button' 
-                                                                        onClick={data.qnty <=1 ?()=>handleDecrement(data.id) :()=>handleSingleDecrement(data)}
+                                                                        <button className='prdct-qty-btn' type='button'
+                                                                            onClick={data.qnty <= 1 ? () => handleDecrement(data.id) : () => handleSingleDecrement(data)}
                                                                         >
                                                                             <i className='fa fa-minus'></i>
                                                                         </button>
                                                                         <input type="text" className='qty-input-box' value={data.qnty} disabled name="" id="" />
-                                                                        <button className='prdct-qty-btn' type='button' onClick={()=>handleIncrement(data)}>
+                                                                        <button className='prdct-qty-btn' type='button' onClick={() => handleIncrement(data)}>
                                                                             <i className='fa fa-plus'></i>
                                                                         </button>
                                                                     </div>
@@ -148,7 +151,7 @@ const CartDetails = () => {
                                             </tr>
                                         </tfoot>
                                     </table>
-                                }
+                            }
                         </div>
                     </div>
                 </div>
